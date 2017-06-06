35a4a719f3a902bf2152ec97c4d2d2428819ce1b
Marcos Sigueros <msigueros@salupro.com>
2017-06-06 16:12:01 +0200

    Updated class doc, and added new test case

--- src/Data.php
+++ src/Data.php
@@ -4,8 +4,13 @@ namespace Alr\ObjectDotNotation;
 class Data
 {
     private $data;
-    private $tram = false;
+    private $part = false;
 
+    /**
+     * Load any object or array
+     * @param $mixed
+     * @return Data
+     */
     static public function load($mixed)
     {
         $d = new Data();
@@ -13,34 +18,41 @@ class Data
         return $d;
     }
 
-    public function l($mixed)
+    private function l($mixed)
     {
         $this->data = json_decode(json_encode($mixed));
     }
 
+    /**
+     * Obtain a specific property value
+     * @param $property
+     * @return bool|null
+     */
     public function get($property)
     {
         $value = null;
         $values = explode('.', $property);
         $first_value = $values[0];
 
-        if($this->tram){
-            if(property_exists($this->tram, $first_value)){
+        if($this->part){
+            if(is_object($this->part) && property_exists($this->part, $first_value)){
                 array_shift($values);
                 $new_prop = implode('.', $values);
-                $this->tram = $this->tram->$first_value;
-                if(is_object($this->tram) || is_array($this->tram)){
+                $this->part = $this->part->$first_value;
+                if(is_object($this->part) || is_array($this->part)){
                     $value = $this->get($new_prop);
                 } else {
-                    $value = $this->tram;
-                    $this->tram = false;
+                    $value = $this->part;
+                    $this->part = false;
                 }
+            } else {
+                $value = $this->part;
             }
         } else {
             if(property_exists($this->data, $first_value)){
                 array_shift($values);
                 $new_prop = implode('.', $values);
-                $this->tram = $this->data->$first_value;
+                $this->part = $this->data->$first_value;
                 $value = $this->get($new_prop);
             }
         }
--- tests/DataTest.php
+++ tests/DataTest.php
@@ -6,10 +6,19 @@ class DataTest extends PHPUnit_Framework_TestCase
 
     public function dataProvider()
     {
+        $element = new stdClass();
+        $array_case = new stdClass();
+        $array_case->collection = [
+            $element, $element
+        ];
+
         return [
             [['two'=>['three'=>'value']], 'two.three', 'value'],
             [['books'=>['alien'=>'no','pan'=>'value']], 'books.alien', 'no'],
-            [(object) ['books'=> ['alien'=>'no','pan'=>'value']], 'books.alien', 'no']
+            [(object) ['books'=> ['alien'=>'no','pan'=>'value']], 'books.alien', 'no'],
+            [ $array_case, 'collection', [
+                $element, $element
+            ]],
         ];
     }
 
@@ -18,10 +27,8 @@ class DataTest extends PHPUnit_Framework_TestCase
      */
     public function testGetter($data, $request, $result)
     {
-        echo json_encode($data);
         $d = Data::load($data);
         $this->assertEquals($result, $d->get($request));
-
     }
 
 }
